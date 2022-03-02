cb7c2081b7513fec47de8ecb88c04aab740824d9
David Castelblanco <cbdavides@gmail.com>
2022-03-02 11:10:32 -0500

    add base nvim and tmux config files

---
+++ nvim/lua/conf.lua
@@ -0,0 +1,14 @@
+require('telescope').setup{
+    defaults = {
+    },
+    extensions = {
+        fzf = {
+            fuzzy = true,                    -- false will only do exact matching
+            override_generic_sorter = false, -- override the generic sorter
+            override_file_sorter = true,     -- override the file sorter
+            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+        }
+    }
+}
+
+require('telescope').load_extension('fzf')
